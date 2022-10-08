c36c53bf38414ee518a8f58905dbf435d9448df5
laoderidho <2081007@unai.edu>
2022-10-08 16:35:09 +0700

    update react Components

---
+++ components/my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import MyButton from './components/myButton'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar></Navbar> 
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello My Name Is Ridho
+        </p>
+        <MyButton></MyButton>
+        <Footer></Footer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/my-app/src/components/myButton.js
@@ -0,0 +1,9 @@
+const myButton = ()=>{
+    return(
+        <div>
+            <button>Click me</button>
+        </div>
+    )
+}
+
+export default myButton;
