d237235d6324c6ba82fa5c65a57d2bf36dcdb173
laoderidho <2081007@unai.edu>
2022-11-01 13:07:00 +0700

    update

--- app1/app1/src/components/BasicExample.jsx
+++ app1/app1/src/components/BasicExample.jsx
@@ -1,29 +1,16 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Row, Col } from "react-bootstrap";
 
-function BasicExample() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+const BasicExample= ()=> {
+  return(
+    <div>
+        <Row>
+          <Col sm={3}>Ini Bagian 1</Col>
+          <Col sm={6}>Ini Bagian 2</Col>
+          <Col sm={3}>Ini Bagian 3</Col>
+        </Row>
+    </div>
+  )
 }
 
 export default BasicExample;
