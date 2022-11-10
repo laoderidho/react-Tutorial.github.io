41f7023dccd3caedc782b1c1332900fbb8104c1c
laoderidho <2081007@unai.edu>
2022-11-10 19:47:00 +0700

    update

---
+++ app1/app1/src/components/UseContext2.jsx
@@ -0,0 +1,11 @@
+import React,{} from "react";
+
+const UseContext2 = ()=>{
+    return(
+        <>
+            <h1>This UseContex</h1>
+        </>
+    )
+}
+
+export default UseContext2;
--- app1/app1/src/components/Usestate.jsx
+++ app1/app1/src/components/Usestate.jsx
@@ -1,8 +1,14 @@
-import React,{ useState } from "react";
+import React,{ useState, useEffect } from "react";
 import {Form, Button} from "react-bootstrap";
 
 const Usestate = ()=>{
-    const [Jumlah, setJumlah] = useState(0); //0 sebagai nilai Jumlah
+    const [Jumlah, setJumlah] = useState(1); //0 sebagai nilai Jumlah
+
+    useEffect(()=>{
+        if(Jumlah <=0){
+            return alert(`Belanjaan Habis`)
+        }
+    }, [Jumlah])
     return (
         <div>
             <p>Jumlah Belanjaan : {Jumlah}</p>
