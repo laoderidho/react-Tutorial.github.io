083c3add3b4325441aed490e277b1f5339ead22b
laoderidho <2081007@unai.edu>
2022-12-14 07:32:47 +0700

    update yesterday

--- app1/app1/src/components/BasicExample.jsx
+++ app1/app1/src/components/BasicExample.jsx
@@ -2,8 +2,6 @@ import { Row, Col } from "react-bootstrap";
 import Usestate from "./Usestate";
 import UseEffect from "./UseEffect";
 import UseReff from "./UseReff";
-import UseContext from "./UseContext";
-import UseContext2 from "./UseContext2";
 
 const BasicExample= ()=> {
   return(
@@ -14,8 +12,6 @@ const BasicExample= ()=> {
           </Col>
           <Col sm={6}>Ini Bagian 2
             <UseEffect></UseEffect>
-            <UseContext />
-            <UseContext2/>
           </Col>
           <Col sm={3}>Ini Bagian 3
             <UseReff></UseReff>
